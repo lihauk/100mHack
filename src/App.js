1d0bae07228c4a1c6a0a76899dad113cbb975e9a
Clarke <paul.clarke@capgemini.com>
2020-09-17 15:07:00 +0200

    small cleanup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
-import React from "react";
-import SimpleTabs from "./components/Tabs";
-import HomePage from "./pages/HomePage";
-import EinoPage from "./pages/EinoPage";
-
-import PaalPage from "./pages/PaalPage";
-
-import LisaPage from "./pages/LisaPage";
-
-// { useState }
+import React from 'react';
+import SimpleTabs from './components/Tabs';
+import HomePage from './pages/HomePage';
+import EinoPage from './pages/EinoPage';
+import PaalPage from './pages/PaalPage';
+import LisaPage from './pages/LisaPage';
 
 const App = () => {
   return (
     <div>
-      {/* sfljksajdf */}
       <div>LOGO $$$Pokemons4cash$$$</div>
       <SimpleTabs>
-        <div Label={"Readme Jenny"}>
-          intro Her skal vi lage:
-          <br />
-          noe
-          <br />
-          Buy a bulbasaur get a free caterpie
-        </div>
-
-        <HomePage Label={"Paul page"} />
-        <HomePage Label={"Pål page"} />
-        <PaalPage Label={"Om Pokemon4Cash"} />
-        <EinoPage Label={"Eino page"} />
-        <LisaPage Label={"Lisa page"} />
-
-        <HomePage Label={"the TEAM page"} />
+        <EinoPage Label={'Eino page'} />
+        <LisaPage Label={'Lisa page'} />
+        <PaalPage Label={'Om Pokemon4Cash'} />
+        <HomePage Label={'the TEAM page'} />
       </SimpleTabs>
     </div>
   );
